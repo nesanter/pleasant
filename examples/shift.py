755034dda1bd13dfd5fc39460f1ee86467616a78
nesanter <santerkrupp@knox.edu>
2015-05-28 10:19:59 -0400

    Add output_ports to Module

--- examples/shift.py
+++ examples/shift.py
@@ -2,12 +2,18 @@ from pleasant.base import Atom
 from pleasant.util import *
 from pleasant.misc import *
 from pleasant.verilog.lang import *
+from pleasant.verilog import generate
 
 
-def make_shift(width,depth):
+def make_shift(width,depth,set_io=True):
     sreg = [bundle(*[Atom("d_in"+str(i)) for i in range(width)])]
+
     for d in range(depth):
         sreg += [reg(bundle(*[Atom("r"+str(i)) for i in range(width)]))]
+
+    if set_io:
+        generate.set_input(sreg[0])
+        generate.set_output(sreg[-1])
 
     shift = []
     for d in range(depth):
--- pleasant/verilog/generate.py
+++ pleasant/verilog/generate.py
@@ -15,55 +15,97 @@
 ## You should have received a copy of the GNU General Public License
 ## along with Pleasant.  If not, see <http://www.gnu.org/licenses/>
 
+import string
+
 import pleasant.util as _util
 from . import lang as _lang
 from . import types as _types
 
 def generic_symbol_stream():
     n = 0
+    obj = (yield None)
     while True:
-        yield "_gen"+str(n)
+        obj = (yield "_gen"+str(n)+"_"+escape(str(obj)))
         n += 1
 
+valid_chars = string.ascii_letters + string.digits
+def escape(s):
+    return str().join([(ch in valid_chars and ch) or '_' for ch in s])
+
 class Module:
-    def __init__(this, *objs, name=None, symbol_stream=None):
+    def __init__(this, *objs, name=None, symbol_stream=None, supress=None):
         this.objs = objs
         this.name = name
         this.symbols = {}
+        if supress == None:
+            this.supress = set()
+        else:
+            this.supress = supress
+
         if symbol_stream == None:
             this.symbol_stream = generic_symbol_stream()
         elif callable(symbol_stream):
             this.symbol_stream = symbol_stream()
         else:
             this.symbol_stream = symbol_stream
+        try:
+            this.symbol_stream.send(None)
+        except TypeError:
+            pass
     def get_symbol(this, obj):
         if not obj in this.symbols:
-            this.symbols[obj] = next(this.symbol_stream)
+            this.symbols[obj] = this.symbol_stream.send(obj)
         return this.symbols[obj]
+    def format_declaration(this, decl):
+        pwidth = decl.attributes.get("bundle_width", 0)
+        upwidth = decl.attributes.get("array_width", ())
+
+        if pwidth == 0:
+            pstr = " "
+        else:
+            pstr = " ["+str(pwidth-1)+":0] "
+
+        if len(upwidth) == 0:
+            upstr = ""
+        else:
+            upstr = " "
+            for w in upwidth:
+                upstr += "["+str(w-1)+":0]"
+        return pstr+this.get_symbol(decl)+upstr
+
     def output_declarations(this, fn=print):
         this.output_declaration_type(_lang.reg, "reg", fn=fn)
         this.output_declaration_type(_lang.wire, "wire", fn=fn)
     def output_declaration_type(this, decltype, declstr, fn=print):
         rdecls = set().union(*[_util.get_by_transform(decltype, obj) for obj in this.objs])
         for decl in rdecls:
-            pwidth = decl.attributes.get("bundle_width", 0)
-            upwidth = decl.attributes.get("array_width", ())
+            if decl.attributes.get("output",False):
+                continue
+            fn(declstr+this.format_declaration(decl))
+    def output_ports(this, fn=print):
+        inputs = set()
+        outputs = set()
+        for obj in this.objs:
+            manual_inputs = {sub for sub in _util.network(obj) if sub.attributes.get("input", False)}
+            ignore = set().union(*[sub.atoms() for sub in manual_inputs])
+            inputs.update({sub for sub in _util.get_inputs(obj) if not sub in ignore}.union(manual_inputs).difference(this.supress))
+            outputs.update({sub for sub in _util.network(obj) if sub.attributes.get("output", False)}.difference(this.supress))
 
-            if pwidth == 0:
-                pstr = " "
+        for i in inputs:
+            fn("input"+this.format_declaration(i))
+        for o in outputs:
+            if _types.reg_t in o.types:
+                fn("output reg"+this.format_declaration(o))
             else:
-                pstr = " ["+str(pwidth-1)+":0] "
-
-            if len(upwidth) == 0:
-                upstr = ""
-            else:
-                upstr = " "
-                for w in upwidth:
-                    upstr += "["+str(w-1)+":0]"
-
-            fn(declstr+pstr+this.get_symbol(decl)+upstr)
+                fn("output"+this.format_declaration(o))
     def __repr__(this):
         if this.name == None:
             return "Module("+str(objs)+")"
         return "Module("+str(this.name)+")"
 
+
+def set_input(obj, is_input=True):
+    obj.attributes["input"] = is_input
+
+def set_output(obj, is_output=True):
+    obj.attributes["output"] = is_output
